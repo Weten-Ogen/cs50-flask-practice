76b39b095beefa20a749e05ddb5fc5bd3fc8015a
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-08-26 19:17:51 +0000

    This is my stack implementation

--- index.py
+++ index.py
@@ -1,7 +1,33 @@
-~~from flask import Flask, render_template, request
+class Empty(Exception):
+  pass
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello world"
+class ArrayStack:
+  def __init__(self):
+    self._data = []
+  
+  def is_empty(self):
+    return len(self._data) == 0
+  
+  def __len__(self):
+    return len(self._data)
+  
+  def top(self):
+    if self.is_empty():
+      raise Empty("The Stack is empty")
+    else:
+      return self._data[-1]
+  
+  def pop(self):
+    if self.is_empty():
+      raise Empty('No element to return')
+    else:
+      return self._data.pop()
+  
+  def push(self,e):
+     return self._data.append(e)
+
+item = ArrayStack()
+item.push(4)
+item.push(5)
+print(item._data)
